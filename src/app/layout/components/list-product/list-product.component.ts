dfa34d075ef8b768cfcc11866f9844dacf7f1a29
Duy Hieu <hieuduy1751@gmail.com>
2022-05-07 22:22:29 +0700

    finished cars and brands manager

--- src/app/layout/components/list-product/list-product.component.ts
+++ src/app/layout/components/list-product/list-product.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../../services/product/product.service";
 import {IProductModel} from "../../../models/i-product-model";
 import {environment} from "../../../../environments/environment";
+import {BrandService} from "../../../services/brand/brand.service";
+import {IBrandModel} from "../../../models/i-brand-model";
 
 
 
@@ -12,9 +14,10 @@ import {environment} from "../../../../environments/environment";
 })
 export class ListProductComponent implements OnInit {
   listProduct: IProductModel[] = [];
+  listBrand: IBrandModel[] = [];
 
-
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private brandService: BrandService) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(res => {
@@ -26,6 +29,13 @@ export class ListProductComponent implements OnInit {
           return;
         }
       });
+    this.brandService.getBrands()
+        .subscribe(res => {
+              this.listBrand = res;
+            },
+            error => {
+              console.log(error);
+            })
   }
 
 }
